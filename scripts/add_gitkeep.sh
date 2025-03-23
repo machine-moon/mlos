1ef30a047a8fe9c9cdf85e3ca8c3e44a7d6c9d97
t4r3k <142579274+machine-moon@users.noreply.github.com>
2025-03-23 17:04:21 -0400

    references

--- scripts/add_gitkeep.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Find all empty directories and add a .gitkeep file in them
-find . -type d -empty -exec touch {}/.gitkeep \;
-
-echo "Added .gitkeep files to all empty directories."
-
